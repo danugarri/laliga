e7a64637b4823241ac1bf85e5df20e603385354c
danugarri <danugarri@hotmail.com>
2023-06-08 19:57:58 +0200

    update useAuth for listening to localStorage changes

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,11 +1,30 @@
 import { useEffect, useState } from 'react';
+
 export const useAuth = () => {
   const [isAuthorised, setIsAuthorised] = useState(false);
 
-  const token = localStorage.getItem('token');
   useEffect(() => {
-    token && token?.length > 0 ? setIsAuthorised(true) : setIsAuthorised(false);
-  }, [token]);
+    const checkIsAuthorised = () => {
+      const token = localStorage.getItem('token');
+      setIsAuthorised(!!token);
+    };
+
+    // Initial check
+    checkIsAuthorised();
+
+    // Listen for changes in localStorage
+    const handleStorageChange = () => {
+      checkIsAuthorised();
+    };
+
+    // Add event listener for storage change
+    window.addEventListener('storage', handleStorageChange);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
 
   return {
     isAuthorised,
