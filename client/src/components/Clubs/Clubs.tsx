ea4763a22497090daf106bddc89b2891068f9469
danugarri <danugarri@hotmail.com>
2023-06-09 23:58:05 +0200

    working both requests with slices and sagas

--- client/src/components/Clubs/Clubs.tsx
+++ client/src/components/Clubs/Clubs.tsx
@@ -1,23 +1,38 @@
 import { useEffect } from 'react';
-
 import { useAppDispatch } from '../../store';
 import { useSelector } from 'react-redux';
 import { selectClubs, selectClubsStatus } from '../../sagas/clubs/clubs.selectors';
+import { Box, Image } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
+import { Club } from './clubs.types';
 
 export const Clubs = () => {
   const clubs = useSelector(selectClubs);
   const clubsStatusRequest = useSelector(selectClubsStatus);
   const dispatch = useAppDispatch();
+
   useEffect(() => {
     dispatch({ type: 'clubs/getAsyncClubs' });
   }, [dispatch]);
+
   return (
     <div>
       {clubsStatusRequest === 'resolved' ? (
-        clubs.map((club) => <p key={club.id}>{club.name}</p>)
+        clubs.map((club) => <section key={club.id}>{newFunction(club)}</section>)
       ) : (
         <h1>Loading</h1>
       )}
     </div>
   );
+
+  function newFunction(club: Club) {
+    return (
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg" margin={4}>
+        <Image src={club.avatar} alt="club" borderRadius="full" boxSize="70px" />
+        <p>{club.name}</p>
+        <p>{club.foundationDate}</p>
+        <span>{club.favorite && <StarIcon />}</span>
+      </Box>
+    );
+  }
 };
