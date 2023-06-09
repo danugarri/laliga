d9d22fae7610d44ba0a675c78ab5bd36b6ac2e40
danugarri <danugarri@hotmail.com>
2023-06-09 21:15:45 +0200

    managin logout when token expires

---
+++ client/src/helpers/helpers.ts
@@ -0,0 +1,24 @@
+export const checkExpirationTime = () => {
+  let isExpired = false;
+  const tokenStored = localStorage.getItem('token')!;
+  if (tokenStored) {
+    //   extract the jwt payload
+    const payload = tokenStored.split('.')[1];
+    // Decoding the Base64-encoded payload
+    const decodedPayload = atob(payload);
+    const parsedPayload = JSON.parse(decodedPayload);
+
+    const expirationTime = parsedPayload.exp;
+    // Multiplying by 1000 to convert from seconds to milliseconds
+    // const humanDate = new Date(parsedPayload.exp * 1000);
+    // console.log(humanDate);
+    // Check if the token has expired
+    const currentTime: number = Math.floor(Date.now() / 1000); // Convert to seconds
+    if (expirationTime && expirationTime < currentTime) {
+      console.log('Token has expired');
+      isExpired = true;
+    }
+  }
+
+  return isExpired;
+};
--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,17 +1,24 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectToken } from '../features/auth/auth.selectors';
+import { checkExpirationTime } from '../helpers/helpers';
 
 export const useAuth = () => {
   const [isAuthorised, setIsAuthorised] = useState(!!localStorage.getItem('token'));
 
+  const isExpired = checkExpirationTime();
   const token = useSelector(selectToken);
+
   useEffect(() => {
     if (token) {
       localStorage.setItem('token', token);
       setIsAuthorised(!!token);
     }
-  }, [token]);
+    if (isExpired) {
+      localStorage.clear();
+      setIsAuthorised(false);
+    }
+  }, [token, isExpired]);
 
   return {
     isAuthorised,
