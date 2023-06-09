ccf79d003baea73d169ab54440c2b3a3768f4570
danugarri <danugarri@hotmail.com>
2023-06-10 00:28:23 +0200

    UPDATED LIBRARY VERSIONchakra styles

--- client/src/components/Clubs/Clubs.tsx
+++ client/src/components/Clubs/Clubs.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useAppDispatch } from '../../store';
 import { useSelector } from 'react-redux';
 import { selectClubs, selectClubsStatus } from '../../sagas/clubs/clubs.selectors';
-import { Box, IconButton, Image, Text } from '@chakra-ui/react';
+import { Box, Image, Text, Icon, Card, CardBody } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
 import { Club } from './clubs.types';
 
@@ -27,18 +27,17 @@ export const Clubs = () => {
 
   function newFunction(club: Club) {
     return (
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg" margin={4}>
-        <Image src={club.avatar} alt="club" borderRadius="full" boxSize="70px" />
-        <Text fontSize="md">{club.name}</Text>
-        <Text fontSize="sm">{club.foundationDate}</Text>
+      <Card>
+        <CardBody>
+          <Box maxW="sm" borderWidth="1px" borderRadius="lg" margin={4}>
+            <Image src={club.avatar} alt="club" borderRadius="full" boxSize="70px" />
+            <Text fontSize="md">{club.name}</Text>
+            <Text fontSize="sm">{club.foundationDate}</Text>
 
-        <IconButton
-          colorScheme={club.favorite === true ? 'blue' : 'red'}
-          aria-label="Call Segun"
-          size="md"
-          icon={<StarIcon />}
-        />
-      </Box>
+            <Icon as={StarIcon} w={8} h={8} color={club.favorite === true ? 'orange' : 'grey'} />
+          </Box>
+        </CardBody>
+      </Card>
     );
   }
 };
