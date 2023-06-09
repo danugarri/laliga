32b5939aefe8150168cd723cc4a3a09862605998
danugarri <danugarri@hotmail.com>
2023-06-10 00:35:52 +0200

    correct components from Chakra

--- client/src/components/Clubs/Clubs.tsx
+++ client/src/components/Clubs/Clubs.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useAppDispatch } from '../../store';
 import { useSelector } from 'react-redux';
 import { selectClubs, selectClubsStatus } from '../../sagas/clubs/clubs.selectors';
-import { Box, Image, Text, Icon, Card, CardBody } from '@chakra-ui/react';
+import { Box, Image, Text, Icon, Card, CardBody, Flex, Spacer } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
 import { Club } from './clubs.types';
 
@@ -28,14 +28,16 @@ export const Clubs = () => {
   function newFunction(club: Club) {
     return (
       <Card>
-        <CardBody>
-          <Box maxW="sm" borderWidth="1px" borderRadius="lg" margin={4}>
-            <Image src={club.avatar} alt="club" borderRadius="full" boxSize="70px" />
-            <Text fontSize="md">{club.name}</Text>
-            <Text fontSize="sm">{club.foundationDate}</Text>
-
+        <CardBody maxW="sm" borderWidth="1px" borderRadius="lg" margin={4}>
+          <Flex>
+            <Box>
+              <Image src={club.avatar} alt="club" borderRadius="full" boxSize="70px" />
+              <Text fontSize="md">{club.name}</Text>
+              <Text fontSize="sm">{club.foundationDate}</Text>
+            </Box>
+            <Spacer />
             <Icon as={StarIcon} w={8} h={8} color={club.favorite === true ? 'orange' : 'grey'} />
-          </Box>
+          </Flex>
         </CardBody>
       </Card>
     );
