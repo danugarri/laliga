1cc3842230f090291bfc08f050e24e87f47e07b5
danugarri <danugarri@hotmail.com>
2023-06-10 04:03:39 +0200

    Added debounce filter for club-name

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -1,15 +1,30 @@
+import React, { useState } from 'react';
 import { FormControl, FormLabel, Input, Switch } from '@chakra-ui/react';
+import { debounce } from 'lodash'; // Importa la función `debounce` de Lodash o la biblioteca que prefieras
 import { FiltersType } from '../../sagas/clubs/clubs.sagas';
 
 export type FormProps = {
   updateFilters: (filters: FiltersType, filter: string, value: any) => void;
   filters: FiltersType;
 };
+
 export const Form = ({ filters, updateFilters }: FormProps) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const debouncedSearch = debounce((value) => {
+    updateFilters(filters, 'name_like', value);
+  }, 3000);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSearchValue(value);
+    debouncedSearch(value);
+  };
+
   return (
     <FormControl>
       <FormLabel>Nombre del club</FormLabel>
-      <Input type="text" onChange={(e) => updateFilters(filters, 'name_like', e.target.value)} />
+      <Input type="text" onChange={handleSearchChange} value={searchValue} />
       <FormLabel>Filtrar por favoritos</FormLabel>
       <Switch
         id="filter-by-favorites"
